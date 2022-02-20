874850b722b9412bd066681fe334426e275fd7c3
fedegy <fede88662@gmail.com>
2022-02-20 17:52:51 -0600

    Se agregaron interfaces para expresion e instruccion

---
+++ Analizador/Ast/Abstract/Interface.go
@@ -0,0 +1,13 @@
+package abstract
+
+import (
+	entorno "OLC2_PROYECTO1_201901073/Analizador/Entorno"
+)
+
+type Expresion interface {
+	GetValor(env entorno.Entorno) RetornoTipo
+}
+
+type Instruccion interface {
+	Ejecutar(env entorno.Entorno) interface{}
+}
