7c978bc57e2367d2b8f22560bb7782d936be8cef
fedegy <fede88662@gmail.com>
2022-02-20 22:49:07 -0600

    Se agrego lista para guardar errores

--- Complementos/Errores.go
+++ Complementos/Errores.go
@@ -13,3 +13,11 @@ type CustomErrorListener struct {
 	*antlr.DefaultErrorListener
 	Errores []CustomError
 }
+
+func (c *CustomErrorListener) CustomError(rec antlr.Recognizer, offsimbol interface{}, linea, columna int, desc string, e antlr.RecognitionException) {
+	c.Errores = append(c.Errores, CustomError{
+		linea:   linea,
+		columna: columna,
+		desc:    desc,
+	})
+}
