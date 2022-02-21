398bbc25c365ed14df54ff1747063c310940b273
fedegy <fede88662@gmail.com>
2022-02-20 18:01:03 -0600

    Se agrego expresion primitiva

---
+++ Analizador/Ast/Expresion/Primitivo.go
@@ -0,0 +1,23 @@
+package expresion
+
+import (
+	abstract "OLC2_PROYECTO1_201901073/Analizador/Ast/Abstract"
+	entorno "OLC2_PROYECTO1_201901073/Analizador/Entorno"
+)
+
+type Primitivo struct {
+	Valor interface{}
+	Tipo  abstract.TipoDato
+}
+
+func (p Primitivo) GetValor(env entorno.Entorno) abstract.RetornoTipo {
+	return abstract.RetornoTipo{
+		Tipo:  p.Tipo,
+		Valor: p.Valor,
+	}
+}
+
+func ValPrimitivo(valor interface{}, tipo abstract.TipoDato) Primitivo {
+	e := Primitivo{valor, tipo}
+	return e
+}
