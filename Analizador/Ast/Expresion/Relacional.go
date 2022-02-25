2475d3fe9d99cd49cb9f30ecae69e772bf193d81
fedegy <fede88662@gmail.com>
2022-02-25 11:43:05 -0600

    Add mayor igual (>=)

--- Analizador/Ast/Expresion/Relacional.go
+++ Analizador/Ast/Expresion/Relacional.go
@@ -45,6 +45,15 @@ func (p Relacional) GetValor(env entorno.Entorno) entorno.RetornoTipo {
 				return entorno.RetornoTipo{Tipo: entorno.BOOL, Valor: (valIzq.Valor.(string) < valDer.Valor.(string))}
 			}
 		}
+	case ">=": {
+		if valIzq.Tipo == entorno.INT && valDer.Tipo == entorno.INT {
+			return entorno.RetornoTipo{Tipo: entorno.BOOL, Valor: (valIzq.Valor.(int) >= valDer.Valor.(int))}
+		} else if valIzq.Tipo == entorno.FLOAT && valDer.Tipo == entorno.FLOAT {
+			return entorno.RetornoTipo{Tipo: entorno.BOOL, Valor: (valIzq.Valor.(float64) >= valDer.Valor.(float64))}
+		} else if valIzq.Tipo == entorno.STRING && valDer.Tipo == entorno.STRING {
+			return entorno.RetornoTipo{Tipo: entorno.BOOL, Valor: (valIzq.Valor.(string) >= valDer.Valor.(string))}
+		}
+	}
 	}
 	return entorno.RetornoTipo{Tipo: entorno.NULL, Valor: nil}
 }
